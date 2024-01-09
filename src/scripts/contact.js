8bfd3b5eb7fa38e7e69c528168910c1a7cb4a0c9
Mr-S4mura1 <david.s4mura1@gmail.com>
2024-01-09 16:17:19 -0500

    Responsive

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,15 +1,14 @@
 import * as clipboard from "clipboard-polyfill";
 
-var copy = document.getElementById("copy")
-var message = document.getElementById("mss")
+var copy = document.getElementById("copy");
+var message = document.getElementById("mss");
 
 copy.addEventListener("click", () => {
-    clipboard.writeText("david.s4mura1@gmail.com")
-    message.style.opacity = "1"
-    message.style.visibility = "visible"
-    setTimeout(() => {
-     message.style.opacity = "0";
+  clipboard.writeText("david.s4mura1@gmail.com");
+  message.style.opacity = "1";
+  message.style.visibility = "visible";
+  setTimeout(() => {
+    message.style.opacity = "0";
     message.style.visibility = "hidden";
-    }, 3000 )
-    
-})
+  }, 3000);
+});
